6cda55595084bc4fb561fd43a122722ea45990f9
Ahmed Elsankary <elsankary99@gmail.com>
2023-09-21 02:17:53 +0300

    adding app images and strings

---
+++ lib/core/constant/app_strings.dart
@@ -0,0 +1,79 @@
+abstract class AppStrings {
+  //ToDo:All Strings
+
+  static const String appName = "Dalel",
+      //!On Boarding Strings
+      skip = "Skip",
+      next = "Next",
+      createAccount = "Create Account",
+      loginNow = "Login Now",
+      onboardingTitle1 = "Explore The history with Dalel in a smart way",
+      onboardingSubTitle1 =
+          "Using our app’s history libraries you can find many historical periods ",
+      onboardingTitle2 = "From every place on earth",
+      onboardingSubTitle2 =
+          "A big variety of ancient places from all over the world",
+      onboardingTitle3 =
+          "Using modern AI technology for better user experience",
+      onboardingSubTitle3 =
+          "AI provide recommendations and helps you to continue the search journey",
+      //!Auth Strings
+      welcome = "Welcome!",
+      welcomeBack = "Welcome Back!",
+      fristName = "First Name",
+      lastName = "Last Name",
+      emailAddress = "Email Address",
+      password = "Password",
+      iHaveAgreeToOur = "I have agree to our ",
+      termsAndCondition = "Terms and Condition",
+      signUp = "Sign Up",
+      signIn = "Sign In",
+      alreadyHaveAnAccount = "Already have an account ?",
+      dontHaveAnAccount = "Don’t have an account ?",
+      forgotPassword = "Forgot Password ?",
+      //!Home Strings
+      recommendations = "Recommendations",
+      aboutAncientEgypt = "About Ancient Egypt",
+      ancientEgyptWars = "Ancient Egypt Wars",
+      //!Search Strings
+      searchDeepIntoTheHistory = "Search deep into the history",
+      recent = "Recent",
+      //!Profile Strings
+      profile = "Profile",
+      account = "Account",
+      editProfile = "Edit Profile",
+      notification = "Notification",
+      general = "General",
+      settings = "Settings",
+      security = "Security",
+      privacyPolicy = "Privacy Policy",
+      logOut = "Log Out",
+      //!Bazar
+      bazar = "Bazar",
+      historyBooks = "History Books",
+      historicalSouvenirs = "Historical Souvenirs",
+      //!Cart
+      myCart = "My Cart",
+      totalPayment = "Total Payment",
+      checkoutNow = "Checkout Now",
+      //!Payment Strings
+      checkoutScreen = "Checkout Screen",
+      deliveryAddress = "Delivery Address",
+      change = "Change",
+      items = "1 items",
+      selectedProduct = "Selected Product",
+      paymentMethod = "Payment Method",
+      confirmPayment = "Confirm Payment",
+      orderSuccess = "Order Success",
+      thankYou =
+          "Thank you for your order here and your package will be sent to your address very quickly and fast good product",
+      backToHome = "Back To Home",
+      //!Forgot password
+      forgotPasswordPage = "Forgot Password",
+      sendVerificationCode = "Send Verification Code",
+      verificationNow = "Verification Now",
+      resendCode = "Resend Code",
+      verifyAccount = "Verify Account",
+      enter4DigitCodeWeHaveSentTo = "Enter 4 digit code we have sent to ",
+      haventReceivedVerificationCode = "Haven’t received verification code?";
+}
